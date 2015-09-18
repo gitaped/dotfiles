5a45bbd775ce8f1205f75dfeb544c93a52e7c60b
Ansley Peduru <ansleypeduru96@gmail.com>
2015-09-18 09:30:00 -0400

    fix indentation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ YCM="YouCompleteMe"
 plugins=( Vundle.vim nerdtree syntastic vim-surround vim-airline supertab
 vim-gitgutter vim-signify vim-fugitive fonts )
 repos=( gmarik scrooloose scrooloose tpope bling ervandew airblade mhinz tpope
-powerline )  
+powerline )
 total=${#plugins[*]}
 
 get_sudo(){
@@ -14,9 +14,9 @@ get_sudo(){
 
 copy_dots(){
 	echo "Copying dotfiles"
-	cp gitconfig "$HOME/.gitconfig"	
+	cp gitconfig "$HOME/.gitconfig"
 	cp bash_aliases "$HOME/.bash_aliases"
-	source ~/.bash_aliases 
+	. ~/.bash_aliases #identical to source 
 	echo
 }
 
@@ -26,7 +26,7 @@ create_symlinks(){
 	rm -rf  ~/.vim
 
 	echo "Creating symlinks to new location"
- 	ln -sf ~/dotfiles/vimrc ~/.vimrc
+	ln -sf ~/dotfiles/vimrc ~/.vimrc
 	ln -sf ~/dotfiles/vim ~/.vim
 	echo
 }
@@ -40,31 +40,31 @@ install_plugins(){
 		echo "bundle directory already created"
 	fi
 	cd $BUNDLE
-	
+
 	for (( p=0; p<=(( $total -1 )); p++ ))
 	do
-	echo "Installing ${plugins[$p]}"
-	if [ ! -d "${plugins[$p]}" ]
-	then
-		git clone $GITHUB/"${repos[$p]}"/"${plugins[$p]}".git
-	else
-		echo "${plugins[$p]} already cloned"
-	fi
+		echo "Installing ${plugins[$p]}"
+		if [ ! -d "${plugins[$p]}" ]
+		then
+			git clone $GITHUB/"${repos[$p]}"/"${plugins[$p]}".git
+		else
+			echo "${plugins[$p]} already cloned"
+		fi
 	done
 
 	#echo "Installing YouCompleteMe"
 	#sudo apt-get install build-essential cmake
 	#sudo apt-get install python-dev
-	
+
 	#if [ ! -d $YCM ]; then
-	#	git clone $GITHUB/Valloric/$YCM.git 
+	#	git clone $GITHUB/Valloric/$YCM.git
 	#else
 	#	echo "YouCompleteMe already cloned"
 	#fi
 	#cd $BUNDLE/YouCompleteMe
 	#git submodule update --init --recursive
-	#./install.py --clang-completer	
-	
+	#./install.py --clang-completer
+
 	vim +PluginInstall +qall
 	cd $BUNDLE/fonts
 	bash install.sh
