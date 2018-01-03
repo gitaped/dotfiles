3971f8d4298d95bb26a47307eb311e97c861fbe5
Ansley Peduru <ansleypeduru96@gmail.com>
2018-01-03 00:29:42 -0500

    Update

--- install.sh
+++ install.sh
@@ -13,13 +13,17 @@ echo "Creating directories"
 mkdir -p ~/src ~/notes
 
 echo "Updating Packages Database"
-apt-get -qqq update
+apt-get update
 
 echo "Installing essential Debian packages"
-cat "$DOTFILES"/packages.list | xargs apt-get -qqq --yes install
+cat $DOTFILES/packages.list | xargs apt-get --yes install
 
 echo "Installing global Python packages"
-sudo pip install -r "$DOTFILES"/requirements.txt
+pip install -r $DOTFILES/requirements.txt
+
+echo "Installing Rust"
+curl https://sh.rustup.rs -sSf | bash -s -- -y
+cat $DOTFILES/cargo.list | xargs cargo install
 
 echo "Deleting exisiting dot configurations"
 rm -f ~/.vimrc
@@ -28,25 +32,25 @@ rm -f ~/.bashrc
 rm -f ~/.tmux.conf
 
 echo "Creating symlinks to new locations"
-ln -sf "$DOTFILES"/vimrc ~/.vimrc
-ln -sf "$DOTFILES"/vim ~/.vim
-ln -sf "$DOTFILES"/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES/vimrc ~/.vimrc
+ln -sf $DOTFILES/vim ~/.vim
+ln -sf $DOTFILES/tmux.conf ~/.tmux.conf
 
-ln -sf "$GIT"/gitconfig ~/.gitconfig
-cp "$GIT"/git-prompt.sh ~/.git-prompt.sh
+ln -sf $GIT/gitconfig ~/.gitconfig
+cp $GIT/git-prompt.sh ~/.git-prompt.sh
 source ~/.git-prompt.sh
 
-ln -sf "$BASH"/bash_aliases ~/.bash_aliases
-ln -sf "$BASH"/bash_profile ~/.bash_profile
-ln -sf "$BASH"/bash_functions ~/.bash_functions
-ln -sf "$BASH"/bashrc ~/.bashrc
+ln -sf $BASH/bash_aliases ~/.bash_aliases
+ln -sf $BASH/bash_profile ~/.bash_profile
+ln -sf $BASH/bash_functions ~/.bash_functions
+ln -sf $BASH/bashrc ~/.bashrc
 source ~/.bashrc
 source ~/.bash_aliases
 source ~/.bash_functions
 source ~/.bash_profile
 
 mkdir -p ~/.config/terminator
-ln -sf "$DOTFILES"/terminator_config ~/.config/terminator/config
+ln -sf $DOTFILES/terminator_config ~/.config/terminator/config
 
 echo "Installing Vim plugins"
 vim +PlugInstall +qall
