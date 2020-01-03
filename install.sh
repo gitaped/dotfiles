23d7fb40c7c05f0705fcf0a26896bcef971dbcc8
Ansley Peduru <ansleypeduru96@gmail.com>
2020-01-02 16:41:17 -0800

    Modify install script

--- install.sh
+++ install.sh
@@ -13,10 +13,16 @@ BASH=$DOTFILES/bash
 # curl https://sh.rustup.rs -sSf | bash -s -- -y
 # cat $DOTFILES/cargo.list | xargs cargo install
 
-# apt update
-# cat $DOTFILES/packages.list | xargs sudo apt-get --yes install
-# mkdir -p ~/.config/terminator
-# ln -sf $DOTFILES/terminator_config ~/.config/terminator/config
+if [ "$(uname)" == "Darwin"  ]; then
+  brew update
+  cat $DOTFILES/macos.list | xargs brew install
+  # TODO: set iterm configuration
+else
+  apt update
+  cat $DOTFILES/packages.list | xargs sudo apt-get --yes install
+  mkdir -p ~/.config/terminator
+  ln -sf $DOTFILES/terminator_config ~/.config/terminator/config
+fi
 
 
 mkdir -p ~/src
@@ -30,8 +36,13 @@ ln -sf $DOTFILES/vim ~/.vim
 ln -sf $DOTFILES/tmux.conf ~/.tmux.conf
 
 ln -sf $GIT/gitconfig ~/.gitconfig
-cp $GIT/git-prompt.sh ~/.git-prompt.sh
-source ~/.git-prompt.sh
+
+# Set local git config for the dotfiles repo
+git config user.name "Ansley Peduru"
+git config user.email "ansleypeduru96@gmail.com"
+
+# cp $GIT/git-prompt.sh ~/.git-prompt.sh
+# source ~/.git-prompt.sh
 
 vim +PlugInstall +qall
 
