6330c2b6d0bba7c7ec5b08acaaa0555eed087b58
Ansley Peduru <ansleypeduru96@gmail.com>
2021-09-11 18:09:08 -0700

    Revamp journal

--- config/fish/functions/journal.fish
+++ config/fish/functions/journal.fish
@@ -1,31 +1,51 @@
 set -x -U JOURNAL_PATH $HOME/journal
 
 function journal
-    if test (count $argv) -gt 1;
-        echo "journal takes 1 argument"
-        return
+    if ! test -d $JOURNAL_PATH;
+        echo "JOURNAL_PATH does not exist"
+        return 1
     end
 
-    set --local dt (date +"%Y-%m-%d")
-    set --local path $JOURNAL_PATH/(date +"%Y")/(date +"%m")
-    set --local entry $path/(date +"%d").md
+    if test (count $argv) -gt 1;
+        echo "too many arguments"
+        return 1
+    end
+
+    # Hashmap implementation (not yet avaiable in fish)
+    # Mapping ranges to time in days
+    set --local range_w 7
+    set --local range_s 14
+    set --local range_m 31
+    set --local range_q 90
+
+    set --local dt (date +"%Y/%m/%d")
+    set --local entry $JOURNAL_PATH/$dt.md
 
     switch "$argv"
-        case -y --yesterday
-            set --local yesterday_entry $JOURNAL_PATH/(date -v-1d +"%Y/%m/%d").md 
-            test ! -e $yesterday_entry || cat $yesterday_entry | less
-        case -f --fortnight
-            echo "fortnight"
-        case -m --month
-            echo "month"
-        case -q --quarter
-            echo "quarter"
-        case \*
-            mkdir -p $path
+        case ""
+        # today
             touch -c $entry
-            test -s $entry || echo "# $dt" >> $entry
+            test -e $entry || echo "# $dt" >> $entry
             $EDITOR $entry
+        case y
+        # yesterday
+            set --local page $JOURNAL_PATH/(date -v-1d +"%Y/%m/%d").md 
+            test ! -e $page || cat $page | less
+        case w s m q
+        # last {week, sprint, month, quarter}
+            for day in (seq (eval echo '$'range_$argv) 0) # chronological order
+                set --local page $JOURNAL_PATH/(date -v-"$day"d +"%Y/%m/%d").md 
+                test ! -e $page && continue || cat $page && echo
+            end | less
+        case commit
+            cd $JOURNAL_PATH
+            git add -A
+            git commit -m $dt
+            git push origin HEAD
+        case \*
+            echo "usage: journal [y w s m q commit]"
+            return 1
     end
+
+    return 0
 end
-
-
