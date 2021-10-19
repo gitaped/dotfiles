3742f66c72d37291aa4b0bb2b29fecc4d2bacfc3
Ansley Peduru <ansleypeduru96@gmail.com>
2021-10-18 18:54:48 -0700

    Remove check for more than 1 args and bake into switch statment

--- config/fish/functions/journal.fish
+++ config/fish/functions/journal.fish
@@ -6,11 +6,6 @@ function journal
         return 1
     end
 
-    if test (count $argv) -gt 1;
-        echo "too many arguments"
-        return 1
-    end
-
     set --local JOURNAL_HELP "usage:
     journal 
     journal [y w s m q]
@@ -28,35 +23,38 @@ function journal
     set --local dt (date +"%Y/%m/%d")
     set --local entry $JOURNAL_PATH/$dt.md
 
-    switch "$argv"
-        case ""
+    if test (count $argv) -eq 0;
         # today
-            mkdir -p (dirname $entry)
-            test -e $entry || echo "# $dt" >> $entry
-            $EDITOR $entry
-        case y
-        # yesterday
-            set --local page $JOURNAL_PATH/(date -v-1d +"%Y/%m/%d").md 
-            test ! -e $page || cat $page | less
-        case w s m q
-        # last {week, sprint, month, quarter}
-            for day in (seq (eval echo '$'range_$argv) 0) # chronological order
-                set --local page $JOURNAL_PATH/(date -v-"$day"d +"%Y/%m/%d").md 
-                test ! -e $page && continue || cat $page && echo
-            end | less
-        case commit
-            if test (git -C $JOURNAL_PATH status --porcelain | wc -l) -gt 0
-                git -C $JOURNAL_PATH add -A
-                git -C $JOURNAL_PATH commit -m $dt
-                git -C $JOURNAL_PATH push origin HEAD
-            else
-                echo "no entries to commit"
+        mkdir -p (dirname $entry)
+        test -e $entry || echo "# $dt" >> $entry
+        $EDITOR $entry
+    else
+        switch $argv[1]
+            case y
+            # yesterday
+                set --local page $JOURNAL_PATH/(date -v-1d +"%Y/%m/%d").md 
+                test ! -e $page || cat $page | less
+            case w s m q
+            # last {week, sprint, month, quarter}
+                for day in (seq (eval echo '$'range_$argv) 0) # chronological order
+                    set --local page $JOURNAL_PATH/(date -v-"$day"d +"%Y/%m/%d").md 
+                    test ! -e $page && continue || cat $page && echo
+                end | less
+            case commit
+                if test (git -C $JOURNAL_PATH status --porcelain | wc -l) -gt 0
+                    git -C $JOURNAL_PATH add -A
+                    git -C $JOURNAL_PATH commit -m $dt
+                    git -C $JOURNAL_PATH push origin HEAD
+                else
+                    echo "no entries to commit"
+                    return 1
+                end
+            case \* or help
+                echo $JOURNAL_HELP
                 return 1
-            end
-        case \* or help
-            echo $JOURNAL_HELP
-            return 1
+        end
     end
+
 
     return 0
 end
