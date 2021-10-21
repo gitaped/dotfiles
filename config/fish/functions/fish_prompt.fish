b59324903948deec8c48e3ab06aeb158d2ec9d29
Ansley Peduru <ansleypeduru96@gmail.com>
2021-10-20 21:16:46 -0700

    Remove setting __fish_prompt_hostname

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,9 +1,4 @@
 function fish_prompt
-    # define hostname if not already defined
-    if not set -q __fish_prompt_hostname
-        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-    end
-
     set _this_machine (uname -s)
     if test "$_this_machine" = "Darwin"
         set -g _workspace (set_color normal)"macbook "
