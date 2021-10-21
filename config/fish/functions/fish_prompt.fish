450b2f0c2562b366dc583bfd98c1745f660fb5d5
Ansley Peduru <ansleypeduru96@gmail.com>
2021-10-21 13:56:11 -0700

    Move __workspace out to own separate function

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,11 +1,5 @@
 function fish_prompt
-    if test (uname -s) = "Darwin"
-        set -g _workspace (set_color normal)"macbook "
-    else
-        set -g _workspace (set_color normal)"desktop "
-    end
-
-    echo -s $_workspace (__kube_status)
+    echo -s (__workspace) (__kube_status)
     echo -s (set_color cyan)(prompt_pwd) (set_color normal)(__fish_git_prompt)
     echo -e (set_color normal)'$ '
 end
