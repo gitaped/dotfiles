924b8d3ab193f8dad2014f7c16561d7e9adccb44
Ansley Peduru <ansleypeduru96@gmail.com>
2022-01-01 19:40:44 -0800

    Get the default branch to track for `gnb`

--- config/fish/functions/gnb.fish
+++ config/fish/functions/gnb.fish
@@ -1,4 +1,5 @@
-function gnb -d "Quickly create a new git branch tracking master"
-    git checkout --track -b "ap/$argv[1]/$argv[2]" origin/master
+function gnb -d "Quickly create a new git branch tracking the default branch"
+    set --local default (git rev-parse --abbrev-ref --symbolic-full-name @{upstream})
+    git checkout --track $default -b "ap/$argv[1]/$argv[2]"
 end
 
