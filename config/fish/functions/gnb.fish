8a4502e458fe7acff24fe86de3141e4ab9e1b6d6
Ansley Peduru <ansleypeduru96@gmail.com>
2022-01-16 13:36:36 -0800

    Remove gnb fish function

--- config/fish/functions/gnb.fish
+++
@@ -1,5 +0,0 @@
-function gnb -d "Quickly create a new git branch tracking the default branch"
-    set --local default (git rev-parse --abbrev-ref --symbolic-full-name @{upstream})
-    git checkout --track $default -b "ap/$argv[1]/$argv[2]"
-end
-
