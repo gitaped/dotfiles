c11c00f68c0cd6a0f4c7dcda2e9f8e5520c21eec
Ansley Peduru <ansleypeduru96@gmail.com>
2022-11-01 23:12:56 -0700

    Replace macbook with apple logo in prompt

--- config/fish/functions/__workspace.fish
+++ config/fish/functions/__workspace.fish
@@ -1,8 +1,8 @@
 function __workspace
     if test (uname -s) = "Darwin"
-        set -g __workspace "macbook "
+        set -g __workspace ""
     else
-        set -g __workspace "desktop "
+        set -g __workspace "desktop"
     end
 
     echo -n -s (set_color normal)$__workspace
